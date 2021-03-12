3df6f1fa2a847c0bd54f99a9f7316a91f343dc71
Askeladd42 <piema.lam@gmail.com>
2021-03-12 11:46:41 +0100

    setup started, metallb deployment to finish

--- setup.sh
+++ setup.sh
@@ -6,12 +6,12 @@
 #    By: plam <plam@student.42.fr>                  +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/03/08 19:27:25 by plam              #+#    #+#              #
-#    Updated: 2021/03/08 19:44:42 by plam             ###   ########.fr        #
+#    Updated: 2021/03/12 11:46:21 by plam             ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 # minikube deployment
-
+minikube delete
 minikube start && minikube kubectl -- get po -A
 kubectl create deployment hello-minikube --image=k8s.gcr.io/echoserver:1.4
 kubectl expose deployment hello-minikube --type=NodePort --port=8080
@@ -19,4 +19,10 @@ kubectl get services hello-minikube
 kubectl port-forward service/hello-minikube 7080:8080
 
 # MetalLB deployment
-kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.7.3/manifests/metallb3.yaml
+kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.9.5/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.9.5/manifests/metallb.yaml
+
+# pod checking
+## kubectl get pod -n metallb-system -o wide
+
+#ConfigMap
